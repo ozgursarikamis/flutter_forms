325f6a60eccaaf53da284a607270beb62b0c5a5b
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-12-13 02:48:10 +0300

    checkbox values with state value and controller

--- lib/widgets/checkbox_input.dart
+++ lib/widgets/checkbox_input.dart
@@ -8,13 +8,23 @@ class CheckboxInput extends StatefulWidget {
 }
 
 class _CheckboxInputState extends State<CheckboxInput> {
+  bool checkBox1 = false;
+  bool checkBox2 = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Checkbox(value: false, onChanged: (bool? value) { print(value); },),
-        Checkbox(value: false, onChanged: (bool? value) { print(value); },),
+        Checkbox(value: checkBox1, onChanged: (bool? value) {
+          setState(() {
+            checkBox1 = !checkBox1;
+          });
+        },),
+        Checkbox(value: checkBox2, onChanged: (bool? value) {
+          setState(() {
+            checkBox2 = !checkBox2;
+          });
+        },),
       ],
     );
   }
