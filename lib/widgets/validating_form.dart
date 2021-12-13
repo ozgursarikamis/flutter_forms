1f32c2013da5b79f348367185486ab68e8214909
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-12-13 04:57:05 +0300

    Validating Form

---
+++ lib/widgets/validating_form.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ValidatingForm extends StatefulWidget {
+  const ValidatingForm({Key? key}) : super(key: key);
+
+  @override
+  _ValidatingFormState createState() => _ValidatingFormState();
+}
+
+class _ValidatingFormState extends State<ValidatingForm> {
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Form(
+            key: formKey,
+            autovalidateMode: AutovalidateMode.always,
+            child: TextFormField(
+              initialValue: "",
+              validator: (String? value) {
+                print("Validator called: $value");
+                if (value!.isEmpty) {
+                  return "Please provide a value";
+                } else {
+                  return null;
+                }
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.validate();
+              },
+              child: const Text("Process"))
+        ],
+      ),
+    );
+  }
+}
