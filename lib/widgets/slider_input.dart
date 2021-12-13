a4d63a38a124ae87faa757bef0ccbbe92d2cd79b
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2021-12-13 03:47:32 +0300

    Slider Input

---
+++ lib/widgets/slider_input.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SliderInput extends StatefulWidget {
+  const SliderInput({Key? key}) : super(key: key);
+
+  @override
+  _SliderInputState createState() => _SliderInputState();
+}
+
+class _SliderInputState extends State<SliderInput> {
+
+  double sliderValue = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Slider(
+        value: sliderValue,
+        min: 0,
+        max: 10,
+        onChanged: (double value) {
+          setState(() {
+            sliderValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
